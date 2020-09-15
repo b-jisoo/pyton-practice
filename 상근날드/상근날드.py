a9318b6d24fd31e650860c94f256f68347c25ec7
jisooo <mrk334kr@naver.com>
2020-09-16 00:24:23 +0900

    상근날드를 파이썬 코드로 작성하였습니다.

---
+++ 상근날드/상근날드.py
@@ -0,0 +1,25 @@
+# Hamburger = list(range(3))
+# drink = list(range(2))
+
+# index = 0
+# for 
+
+
+# index = 1
+
+# for Hamburger in range(1, 4):
+#     Hamburger[index] = int(input())
+#     index += 1
+
+# index = 1
+# for drink in range(1, 3):
+#     drink[index] = int(input())
+#     index += 1
+
+# print( min(Hamburger) + min(drink) - 50 )
+
+Hamburger = [int(input()) for _ in range(3)]
+drink = [int(input()) for _ in range(2)]
+
+print( min(Hamburger) + min(drink) - 50 )
+
