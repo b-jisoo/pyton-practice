f78868685caa054315a1445149daaa8b238f3ecd
jisooo <mrk334kr@naver.com>
2020-09-16 17:01:07 +0900

    세 수
    피드백에 맞춰 수정하였습니다.

--- 세 수/세 수.py
+++ 세 수/세 수.py
@@ -1,13 +1,19 @@
+# A, B, C = map(int, input().split())
+
+
+# if (A>=B and A<=C) or (A>=C and A<=B):
+#     print(A)
+# elif (B>=A and B<=C) or (B>=C and B<=A):
+#     print(B)
+# else:
+#     print(C)
+ #--------------------------------------------------
+
 A, B, C = map(int, input().split())
 
-
-# print(A)
-# print(B)
-# print(C)
-
-if (A>=B and A<=C) or (A>=C and A<=B):
+if C <= A <= B or B <= A <= C :
     print(A)
-elif (B>=A and B<=C) or (B>=C and B<=A):
+elif C <= B <= A or A<= B <= C :
     print(B)
 else:
     print(C)
