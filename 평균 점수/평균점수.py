517b93ec218f76e2dabecb6b2bd77c051829d86b
jisooo <mrk334kr@naver.com>
2020-09-16 17:01:16 +0900

    평균 점수
    피드백에 맞춰 수정하였습니다.

--- 평균 점수/평균점수.py
+++ 평균 점수/평균점수.py
@@ -1,10 +1,24 @@
-score = list(range(5))
+# score = list(range(5))
 
-index = 0
-while(index <= 4):
-    score[index] = int(input())
-    if score[index] < 40:
-        score[index] = 40
-    index += 1
+# index = 0
+# while(index <= 4):
+#     score[index] = int(input())
+#     if score[index] < 40:
+#         score[index] = 40
+#     index += 1
 
-print( int(sum(score)/5) )
+# print( int(sum(score)/5) )
+
+ #--------------------------------------------------
+
+sum = 0 
+for score in range(5):
+    score = int(input())
+    if score < 40:
+        sum += 40
+    else :
+        sum += score
+        
+print(int(sum/5))
+
+
