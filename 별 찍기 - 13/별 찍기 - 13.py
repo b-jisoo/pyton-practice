968eecbd48a702847360f19269491d7c02b02fa2
jisooo <mrk334kr@naver.com>
2020-09-16 16:59:23 +0900

    별 찍기 -13 주석 삭제

--- 별 찍기 - 13/별 찍기 - 13.py
+++ 별 찍기 - 13/별 찍기 - 13.py
@@ -1,33 +1,3 @@
-# num = int(input())
-# star = []
-# for num in range(1, num + 1):
-#     star.append("*")
-#     print(star)
-    
-    
-# num = int(input())
-# star = []
-# i = 0
-# while(i < num ):
-#     star.append("*")
-#     print(star)
-#     i += 1
-#     if(i == num):
-#         while (num > 1):
-#             star.pop()
-#             print(star)
-#             num -= 1
-#         break
-
-
-# num = int(input())
-# star = []
-# for _ in range(num):
-#     star.append("*")
-#     print(star)
-# for _ in range(num-1):
-#     star.pop()
-#     print(star)
 
 num = int(input())
 i = 1
