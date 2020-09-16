c5b7bfb4f111287011dcdca1c9bd0b8bb4221fc8
jisooo <mrk334kr@naver.com>
2020-09-16 16:59:44 +0900

    별 찍기 -21 주석 삭제

--- 별 찍기 - 21/별 찍기 -21.py
+++ 별 찍기 - 21/별 찍기 -21.py
@@ -1,18 +1,4 @@
-# i = int(input())
 
-# while(i >= 0):
-#     if ( i % 2) == 1 :
-#         print("*")
-#     if ( i % 2) == 0 :
-#         print(" " * (i - 1) + "*" * (i - 1))
-#     i -= 1
-
-# i = int(input())
-
-# while(i > 0):   
-#     print("*")
-#     print(" " * ((i - 1) % 2) + "*" * ((i - 1) % 2))
-#     i -= 1
 
 i = int(input())
 for j in range(1, i + 1):
