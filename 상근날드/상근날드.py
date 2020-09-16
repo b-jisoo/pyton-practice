cf67fecc688ff8bbc9253feb2bbbc4b7debaa960
jisooo <mrk334kr@naver.com>
2020-09-16 17:00:53 +0900

    상근날드
    피드백에 맞춰 수정하였습니다.

--- 상근날드/상근날드.py
+++ 상근날드/상근날드.py
@@ -1,25 +1,7 @@
-# Hamburger = list(range(3))
-# drink = list(range(2))
-
-# index = 0
-# for 
 
 
-# index = 1
-
-# for Hamburger in range(1, 4):
-#     Hamburger[index] = int(input())
-#     index += 1
-
-# index = 1
-# for drink in range(1, 3):
-#     drink[index] = int(input())
-#     index += 1
-
-# print( min(Hamburger) + min(drink) - 50 )
-
-Hamburger = [int(input()) for _ in range(3)]
+hamburger = [int(input()) for _ in range(3)]
 drink = [int(input()) for _ in range(2)]
 
-print( min(Hamburger) + min(drink) - 50 )
+print( min(hamburger) + min(drink) - 50 )  # Hamburger -> hamburger 소문자로 변경
 
